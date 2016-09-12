2ee361b9837b9ab815ec93910545b0a2f1db16bf
tteckchandani9481 <ttanmay1193@gmail.com>
2016-09-11 21:52:26 -0400

    Add files via upload

---
+++ Grp1_assign1/Grp1_assign1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿/*This Program displays english word for Latin word
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grp1_assign1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //When this Button is clicked, it shows english word for Sinister
+        private void btn_sinister_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_left.Visibility = Visibility.Visible;
+            lbl_right.Visibility = Visibility.Hidden;
+            lbl_center.Visibility = Visibility.Hidden;
+        }
+
+        //When this Button is clicked, it shows english word for Dexter
+        private void btn_dexter_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_left.Visibility = Visibility.Hidden;
+            lbl_right.Visibility = Visibility.Visible;
+            lbl_center.Visibility = Visibility.Hidden;
+        }
+
+        //When this Button is clicked, it shows english word for Medium
+        private void btn_medium_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_left.Visibility = Visibility.Hidden;
+            lbl_right.Visibility = Visibility.Hidden;
+            lbl_center.Visibility = Visibility.Visible;
+        }
+    }
+}
